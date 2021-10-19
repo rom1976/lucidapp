f4f63bb1756b439b79b9837f76e39fafd41969d7
Romulas Paul <79131482+rom1976@users.noreply.github.com>
2021-10-19 06:00:41 +0530

    Updates

---
+++ precache-manifest.141360c4a2617e526319770f564210c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10474ee81e903062db22319798018159",
+    "url": "/lucidapp/index.html"
+  },
+  {
+    "revision": "801e474f69914863759a",
+    "url": "/lucidapp/static/css/main.47135dd3.chunk.css"
+  },
+  {
+    "revision": "9218a4abab225987b0bd",
+    "url": "/lucidapp/static/js/2.3be0a514.chunk.js"
+  },
+  {
+    "revision": "9ca051d54135a2e92f6e02a503e5bc95",
+    "url": "/lucidapp/static/js/2.3be0a514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2ef0847ebf010cb6dd",
+    "url": "/lucidapp/static/js/3.34e5791f.chunk.js"
+  },
+  {
+    "revision": "801e474f69914863759a",
+    "url": "/lucidapp/static/js/main.7134a528.chunk.js"
+  },
+  {
+    "revision": "b6bead242cab436f18a6",
+    "url": "/lucidapp/static/js/runtime-main.cf1fcfe2.js"
+  }
+]);
